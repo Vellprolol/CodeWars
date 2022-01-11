17a201e3b43ef1ab3feda8c84804bcdd4be84858
vellprolol <vellprolol@gmail.com>
2022-01-11 11:39:55 +0300

    RomanEncoder edit

--- Kata 6/src/com/codewars/romanEncoder/RomanEncoder.java
+++ Kata 6/src/com/codewars/romanEncoder/RomanEncoder.java
@@ -3,8 +3,11 @@ package com.codewars.romanEncoder;
 import java.util.TreeMap;
 
 public class RomanEncoder {
-    public static String encode(int n) {
-        if (n > 3999 || n <= 0) {
+    public static void main(String[] args) {
+        System.out.println( fromRoman("MCDXXIX"));
+    }
+    public static String toRoman(int n) {
+        if (n > 3999 || n < 1) {
             return "";
         }
         TreeMap<Integer, String> map = new TreeMap<>();
@@ -26,6 +29,22 @@ public class RomanEncoder {
         if (n == l) {
             return map.get(n);
         }
-        return map.get(l) + encode(n-l);
+        return map.get(l) + toRoman(n-l);
+    }
+
+    public static int fromRoman(String romanNumeral) {
+        if (!romanNumeral.matches("(M{0,3})(D?C{0,3}|C[DM])(L?X{0,3}|X[LC])(V?I{0,3}|I[VX])")) {
+            return 0;
+        }
+        final String[] numerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        int l = 0;
+        for (int i = 0; i < numerals.length; i++) {
+            while (romanNumeral.startsWith(numerals[i])) {
+                l += values[i];
+                romanNumeral = romanNumeral.substring(numerals[i].length());
+            }
+        }
+        return l;
     }
 }
--- Kata 6/src/com/codewars/romanEncoder/RomanEncoderTest.java
+++ Kata 6/src/com/codewars/romanEncoder/RomanEncoderTest.java
@@ -8,10 +8,14 @@ class RomanEncoderTest {
 
     @Test
     void encode() {
-        assertEquals("MXXI", RomanEncoder.encode(1021));
-        assertEquals("M", RomanEncoder.encode(1000));
-        assertEquals("", RomanEncoder.encode(4000));
-        assertEquals("", RomanEncoder.encode(-1));
-        assertEquals("", RomanEncoder.encode(0));
+        assertEquals("MXXI", RomanEncoder.toRoman(1021));
+        assertEquals("M", RomanEncoder.toRoman(1000));
+        assertEquals("", RomanEncoder.toRoman(4000));
+        assertEquals("", RomanEncoder.toRoman(-1));
+        assertEquals("", RomanEncoder.toRoman(0));
+        assertEquals(1021, RomanEncoder.fromRoman("MXXI"));
+        assertEquals(1000, RomanEncoder.fromRoman("M"));
+        assertEquals(0, RomanEncoder.fromRoman("MDFAXXAJOSAX"));
+        assertEquals(1429, RomanEncoder.fromRoman("MCDXXIX"));
     }
 }
