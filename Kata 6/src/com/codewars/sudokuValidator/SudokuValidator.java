8d3428e24f2b5ef375761a84f9916fb9628a56cf
vellprolol <vellprolol@gmail.com>
2022-01-26 00:21:04 +0300

    Task "Sudoku Validator" added

---
+++ Kata 6/src/com/codewars/sudokuValidator/SudokuValidator.java
@@ -0,0 +1,39 @@
+package com.codewars.sudokuValidator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SudokuValidator {
+    public static boolean validate(int[][] sudoku) {
+        boolean isValid = true;
+        //introduce local variables to split the sudoku into small arrays 3x3
+        int c = 0;
+        int d = 0;
+        int value = 1;
+        while (c < 9 && d < 9) {
+        //creating map to check duplicate values in the small arrays
+            Map<Integer, Integer> sudokuMap = new HashMap<>();
+            for (int i = c; i < c + 3; i++) {
+                for (int j = d; j < d + 3; j++) {
+                    int key = sudoku[i][j];
+                    if (sudokuMap.containsKey(key)) {
+                        sudokuMap.put(key, sudokuMap.get(key) + 1);
+                    } else {
+                        sudokuMap.put(key, value);
+                    }
+                }
+            }
+            for (Map.Entry<Integer, Integer> entry : sudokuMap.entrySet()) {
+                if (entry.getValue() > 1 || entry.getKey() == 0) {
+                    isValid = false;
+                }
+            }
+            d += 3;
+            if (d > 6) {
+                d = 0;
+                c += 3;
+            }
+        }
+        return isValid;
+    }
+}
